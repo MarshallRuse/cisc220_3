23d068783fb6895c9d3ce957f4d35cb2f099bc97
archiveteng <tengsitong@hotmail.com>
2016-11-14 19:54:20 -0500

    Add files via upload

---
+++ question6.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+
+students=($(cat student.txt))
+declare -A numName
+
+for i in $(seq 2 2 $((${#students[*]}-1)) ); do
+	numName[${students[$i]}]=${students[$(($i+1))]}
+done
+
+classes=($(cat course.txt))
+declare -A idClass
+
+for j in $(seq 2 2 $((${#classes[*]}-1)) ); do
+	idClass[${classes[$j]}]=${classes[$(($j+1))]}
+done
+
+mixed=($(cat student_course.txt))
+declare -a results
+
+echo "Output"
+echo -ne "studentName\tcourseName"
+
+for k in $(seq 2 2 $((${#mixed[*]}-1)) ); do
+	studentID=${mixed[$k]}
+	courseID=${mixed[$(($k+1))]}
+	results+=("\n${numName[$studentID]}\t${idClass[$courseID]}")
+done
+
+echo -e "${results[*]}" | sort -k1,1 -k2,2
